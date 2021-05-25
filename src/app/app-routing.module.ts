07e90ecb50635c88a9186fdb0e630c1364383ab4
SHIVANGI <shivangi.gupta@bts.com>
2021-05-26 02:49:17 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MusicDashboardComponent } from './music-dashboard/music-dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'music', component: MusicDashboardComponent
+},
+{ path: '', redirectTo: 'music', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
